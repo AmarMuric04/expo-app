b5aa67b8200eaefb20ccc8d1fe3b5d9d1d964fa2
AmarMuric04 <muricamar2004@gmail.com>
2025-09-10 18:11:01 +0200

    chore(ui): Adds history page with placeholder data

---
+++ src/app/history/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function HistoryScreen() {
+  return (
+    <View className="flex-1">
+      <Text>This is the history page</Text>
+    </View>
+  );
+}
